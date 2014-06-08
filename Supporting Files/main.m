c762e870b12ef54f6079517afd0f26d4c66ece34
Pantazis Deligiannis <pdeligia@me.com>
2014-06-08 12:38:11 +0530

    added Supporting Files

---
+++ Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DungeonStory
+//
+//  Created by Pantazis Deligiannis on 22/09/2012.
+//  Copyright Pantazis Deligiannis 2012-2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+        return retVal;
+    }
+}
